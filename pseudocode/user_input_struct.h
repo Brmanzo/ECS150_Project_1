cf4029dc9b6365e69515616af072de3514205540
Kent Cassidy <kdcassidy@ucdavis.edu>
2022-10-03 12:41:06 -0700

    basic template for expected user input parsing

---
+++ pseudocode/user_input_struct.h
@@ -0,0 +1,26 @@
+struct userInputs {
+
+  char prgrm1[];
+  char prgrm1Args[];
+  bool pipeOut1;
+  bool redirectOut1;
+  
+  char prgrm2[];
+  char prgrm2Args[];
+  bool pipeIn2;
+  bool pipeOut2;
+  bool redirectIn2;
+  bool redirectOut2;
+  
+  char prgrm3[];
+  char prgrm3Args[];
+  bool pipeIn3;
+  bool redirectIn3;
+  
+  //Flags
+  bool flagA;
+  bool flagB;
+  bool flagC;
+  bool flagP;
+  
+};
