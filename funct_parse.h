e2c5ef01cd0be8c7bbfb842fb6caf29823b027a8
Bradley Manzo <brmanzo@ucdavis.edu>
2022-10-04 00:25:01 -0700

    Space_Redir function works

--- funct_parse.h
+++ funct_parse.h
@@ -13,29 +13,48 @@
 char arg_array[ARG_MAX][TOK_LEN_MAX];
 
 // Takes in the command line and the redirect character of choice
-void redir_space(char* cmd[], const char redir)
+char* redir_space(char* cmd)
 {
-  char* redir_pos = 0;
-  size_t cmd_len = strlen(cmd);
+  char buf_cmd[CMDLINE_MAX];
+  int j = 0;
   
-  // Looks for the redirect character and inserts spaces surrounding it
-  redir_pos =  strchr(cmd, redir)
-  memmove(redir_pos +1 , redir_pos, cmd_len - redir_pos);
-  *redir_pos = ' ';
-  memmove(redir_pos +2 , redir_pos + 1, cmd_len - redir_pos - 1);
-  *redir_pos + 2 = ' ';
+  // Looks for the redirect characters < and > inserting spaces surrounding them
+  for(int i = 0; i < strlen(cmd); i++)
+  {
+    if(cmd[i] == '<')
+    {
+        buf_cmd[j] = ' ';
+        j++;
+        buf_cmd[j] = '<';
+        j++;
+        buf_cmd[j] = ' ';
+        j++;
+    }
+    else if(cmd[i] == '>')
+    {
+        buf_cmd[j] = ' ';
+        j++;
+        buf_cmd[j] = '>';
+        j++;
+        buf_cmd[j] = ' ';
+        j++;
+    // Otherwise it simply buffers the character
+    } else {
+        buf_cmd[j] = cmd[i];
+        j++;
+    }
+
+  }
 }
   
-char funct_parse(char[] cmd)
+char funct_parse(char* cmd)
 {
   // Inserts spaces surrounding redirection characters
-  redir_space(cmd, '<');
-  redir_space(cmd, '>');
+  char* buf_arr = redir_space(cmd);
 
-  char[] tokens = " ";
   int arg_num = 0;
   
-  char* cmd_arg = strtok(cmd, tokens);
+  char* cmd_arg = strtok(buf_arr, " ");
   
   strcpy(arg_array[arg_num], cmd_arg);
   arg_num++;
@@ -44,7 +63,7 @@ char funct_parse(char[] cmd)
   {
     strcpy(arg_array[arg_num], cmd_arg);
 
-    cmd_arg = strtok(NULL, tokens);
+    cmd_arg = strtok(NULL, " ");
     arg_num++;
   }
      
