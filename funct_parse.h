65cd7983672f1c4bd4f52bef919bcc44b7fe57e5
Brmanzo <76194522+Brmanzo@users.noreply.github.com>
2022-10-04 11:33:21 -0700

    Maintained parity with funct_parse in test code folder

--- funct_parse.h
+++ funct_parse.h
@@ -12,7 +12,7 @@
 
 
 
-// Takes in the command line and the redirect character of choice
+// Takes in the command line and returns the new array with spaces
 char* redir_space(char* cmd)
 {
   char buf_cmd[CMDLINE_MAX];
@@ -23,27 +23,26 @@ char* redir_space(char* cmd)
   {
     if(cmd[i] == '<')
     {
-        buf_cmd[j] = ' ';
-        j++;
-        buf_cmd[j] = '<';
-        j++;
-        buf_cmd[j] = ' ';
-        j++;
+      buf_cmd[j] = ' ';
+      j++;
+      buf_cmd[j] = '<';
+      j++;
+      buf_cmd[j] = ' ';
+      j++;
     }
     else if(cmd[i] == '>')
     {
-        buf_cmd[j] = ' ';
-        j++;
-        buf_cmd[j] = '>';
-        j++;
-        buf_cmd[j] = ' ';
-        j++;
-    // Otherwise it simply buffers the character
+      buf_cmd[j] = ' ';
+      j++;
+      buf_cmd[j] = '>';
+      j++;
+      buf_cmd[j] = ' ';
+      j++;
+    // Otherwise it simply buffers the character into the new array
     } else {
-        buf_cmd[j] = cmd[i];
-        j++;
+      buf_cmd[j] = cmd[i];
+      j++;
     }
-
   }
 }
   
@@ -56,12 +55,12 @@ void funct_parse(char* cmd, char** arg_array)
   
   char* cmd_arg = strtok(buf_arr, " ");
   
-  strcpy(arg_array[arg_num], cmd_arg);
+  arg_array[arg_num] = cmd_arg;
   arg_num++;
 
   while(cmd_arg != NULL)
   {
-    strcpy(arg_array[arg_num], cmd_arg);
+    arg_array[arg_num] = cmd_arg;
 
     cmd_arg = strtok(NULL, " ");
     arg_num++;
