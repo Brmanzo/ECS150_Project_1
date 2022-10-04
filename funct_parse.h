7f5ef4856ee25efd2cba50792483f717a64e7658
Brmanzo <76194522+Brmanzo@users.noreply.github.com>
2022-10-03 19:35:59 -0700

    Added Redir_Space function

--- funct_parse.h
+++ funct_parse.h
@@ -12,29 +12,40 @@
 
 char arg_array[ARG_MAX][TOK_LEN_MAX];
 
+// Takes in the command line and the redirect character of choice
+void redir_space(char* cmd[], const char redir)
+{
+  char* redir_pos = 0;
+  size_t cmd_len = strlen(cmd);
+  
+  // Looks for the redirect character and inserts spaces surrounding it
+  redir_pos =  strchr(cmd, redir)
+  memmove(redir_pos +1 , redir_pos, cmd_len - redir_pos);
+  *redir_pos = ' ';
+  memmove(redir_pos +2 , redir_pos + 1, cmd_len - redir_pos - 1);
+  *redir_pos + 2 = ' ';
+}
+  
 char funct_parse(char[] cmd)
 {
+  // Inserts spaces surrounding redirection characters
+  redir_space(cmd, '<');
+  redir_space(cmd, '>');
+
   char[] tokens = " ";
   int arg_num = 0;
   
   char* cmd_arg = strtok(cmd, tokens);
   
-  redir = strchr(cmd_arg, '<');
-  if(redir){
-    *redir = '\0';
-    arg_array[arg_num] = cmd_arg;
-    arg_num++;
-    arg_array[arg_num] = <;
-    arg_num++;
-  } else {
-    arg_array[arg_num] = cmd_arg
-    arg_num++;
-  }
-  
+  strcpy(arg_array[arg_num], cmd_arg);
+  arg_num++;
+
   while(cmd_arg != NULL)
   {
-    arg_array[arg_num] = cmd_arg
+    strcpy(arg_array[arg_num], cmd_arg);
+
     cmd_arg = strtok(NULL, tokens);
+    arg_num++;
   }
      
 }
